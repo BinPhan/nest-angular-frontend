2bd547ce84631170549b2b77f04806de005567e4
BinPhan <binalwayssmile@gmail.com>
2022-10-05 18:49:49 +0700

    Update

--- src/app/auth/login.guard.spec.ts
+++ src/app/auth/login.guard.spec.ts
@@ -1,9 +1,30 @@
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router"
+import { StoreService } from "../common/service/store.service";
+import { LoginGuard } from "./login.guard"
 
 describe('LoginGuard', () => {
 
+  let guard: LoginGuard
+  let routerSpy: jasmine.SpyObj<Router>;
+  const dummyRoute = {} as ActivatedRouteSnapshot;
+  const fakeUrls = ['/', '/admin', '/crisis-center', '/a/deep/route'];
+
+  function fakeRouterState(url: string): RouterStateSnapshot {
+    return {
+      url,
+    } as RouterStateSnapshot;
+  }
 
   beforeEach(() => {
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
 
+    guard = new LoginGuard(routerSpy, new StoreService)
+  })
+
+  it('Can active', () => {
+    const canActivate = guard.canActivate(dummyRoute, fakeRouterState('/login'));
+
+    expect(canActivate)
   })
 
 })
